8ecab4dd6dac7ab0b00e07657d7d1a75b91639ba
Alex Pham <925workk@gmail.com>
2021-02-22 15:55:08 -0700

    Added media query at 473 px.

--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import faker from 'faker';
-
+import ApprovalCard from './ApprovalCard';
 import CommentDetails from './CommentDetails';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetails author='Sam' timeAgo='Today at 4:45PM' avatar='https://source.unsplash.com/random' commentContent='Blog post example' />
-            <CommentDetails author='Alex' timeAgo='Yesterday at 4:00PM' avatar='https://source.unsplash.com/random' commentContent='Lorem Ipsum' />
-            <CommentDetails author='Tom' timeAgo='2 days ago' avatar='https://source.unsplash.com/random' commentContent='Taco bell is rad' />
+            <ApprovalCard>
+                <div>Are you sure you want to do this?</div>
+                <CommentDetails author='Sally' timeAgo='Today at 4:50PM' avatar='https://source.unsplash.com/random' commentContent='Sally Sells Sea Shells'></CommentDetails>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetails author='Sam' timeAgo='Today at 4:45PM' avatar='https://source.unsplash.com/random' commentContent='Blog post example' />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetails author='Alex' timeAgo='Yesterday at 4:00PM' avatar='https://source.unsplash.com/random' commentContent='Lorem Ipsum' />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetails author='Tom' timeAgo='2 days ago' avatar='https://source.unsplash.com/random' commentContent='Taco bell is rad' />
+            </ApprovalCard>
         </div>
     );
 };
